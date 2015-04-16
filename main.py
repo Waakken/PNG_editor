9a03b97f74087a0adeb2474800f9a1c938cdaf0a
Jani Kuutvuori <janikuutvuori@hotmail.com>
2015-04-16 23:01:34 +0300

    Added pixel by pixel reconst. Broken commit

--- main.py
+++ main.py
@@ -66,36 +66,152 @@ def write_chunk(outfile, chunkType, data=''):
     checksum = zlib.crc32(data, checksum)
     outfile.write(struct.pack("!i", checksum))
 
-#Hello world -style editing of color bytes
-def editColorBytes(chunkData, dimensions, colorAdj = 0):
+# https://github.com/Alexis-D/png.py/blob/master/png.py
+def paeth(a, b, c):
+    """Simple implementation of http://www.w3.org/TR/PNG/#9Filter-type-4-Paeth
+    (nearly c/p the pseudo-code)
+    """
+    try:
+      a = struct.unpack("B", a)[0]
+    except struct.error:
+      a = 0
+    try:
+      b = struct.unpack("B", b)[0]
+    except struct.error:
+      b = 0
+    try:
+      c = struct.unpack("B", c)[0]
+    except struct.error:
+      c = 0
+    p = a + b - c
+    pa, pb, pc = abs(p - a), abs(p - b), abs(p - c)
+
+    if pa <= pb and pa <= pc:
+        return struct.pack("B", a)
+    elif pb <= pc:
+        return struct.pack("B", b)
+    return struct.pack("B", c)
+
+# Reconstruct one Paeth filtered scanline
+def reconstructPaeth(curRow, lastRow, width):
+  # Recon(x) = Filt(x) + PaethPredictor(Recon(a), Recon(b), Recon(c))
+  newRow = ""
+  newRow += curRow[0]
+  newRow += curRow[1]
+  newRow += curRow[2]
+  #newRow += b'\x00'
+  for i in range(3, (width*3)):
+   
+    filtX = curRow[i]
+
+    try:
+      reconA = struct.unpack("B", curRow[i-3])[0]
+    except struct.error:
+      reconA = 0
+
+    try:
+      reconB = struct.unpack("B", lastRow[i])[0]
+    except struct.error:
+      reconB = 0
+
+    try:
+      reconC = struct.unpack("B", lastRow[i-3])[0]
+    except struct.error:
+      reconA = 0
+
+    reconX = struct.unpack("B", filtX)[0] + struct.unpack("B", paeth(reconA, reconB, reconC))[0]
+    newRow += struct.pack("B", reconX)
+
+  return newRow
+
+# Reconstruct one Sub filtered scanline
+def reconstructSub(curRow, lastRow, width):
+  # Recon(x) = Filt(x) + Recon(a)
+  newRow = ""
+  #newRow += b'\x00'
+  newRow += curRow[0]
+  newRow += curRow[1]
+  newRow += curRow[2]
+  for i in range(3, (width*3)):
+    try:
+      x = struct.unpack("B", curRow[i])[0]
+    except struct.error:
+      x = 0
+    try:
+      a = struct.unpack("B", newRow[i-3])[0]
+    except struct.error:
+      a = 0
+    newX = (a + x)
+    if newX > 255:
+      newX = newX % 256
+    newRow += struct.pack("B", newX)
+    #newRow[i] = curRow[i] + curRow[i-1]
+  return newRow
+
+# Reconstruct one Avg filtered scanline
+def reconstructAvg(curRow, lastRow, width):
+  # Recon(x) = Filt(x) + floor((Recon(a) + Recon(b)) / 2)
+  newRow = ""
+  newRow += curRow[0]
+  newRow += curRow[1]
+  newRow += curRow[2]
+  for i in range(3, (width*3)):
+    try:
+      a = struct.unpack("B", curRow[i-3])[0]
+    except struct.error:
+      a = 0
+    try:
+      b = struct.unpack("B", lastRow[i])[0]
+    except struct.error:
+      b = 0
+    x = round( (a + b) / 2)
+    newRow += struct.pack("B", x)
+    #newRow[i] = curRow[i] + curRow[i-1]
+  return newRow
+
+#Remove filtering from each line
+def reconstData(chunkData, dimensions, colorAdj = 0):
+    print "Reconstructing filtered data.."
     width = dimensions[0]
     height = dimensions[1]
     newChunk = ""
+    lastRow = [0] * width
     chunkData = zlib.decompress(chunkData)
     oldLen = len(chunkData)
-    print "Old data length:", oldLen
     chunkData = StringIO.StringIO(chunkData)
     #chunkData is a string. Each byte containg either R, B or G value
     for i in range(height):
-      newRow = chunkData.read(width*3+1)
-      print "Row %d Filter: %d" % (i, struct.unpack("B", newRow[0])[0])
-    """
-    i = 0
-    for colorVal in chunkData:
-      if (i % 3) == 2:
-        oldVal = struct.unpack("B", colorVal)
-        newVal = oldVal[0] + colorAdj
-        #newVal = colorVal + struct.pack("B", colorAdj)
-        if newVal > 255:
-          newVal = 255
-        newColor = struct.pack("B", newVal)
-        newChunk += newColor
+      newFilter = chunkData.read(1)
+      newRow = chunkData.read(width*3)
+      oldRowLen = len(newRow)
+      #print "Debug: Row %d Filter: %d" % (i, struct.unpack("B", newFilter[0])[0])
+      if newFilter == b'\x00':
+        raise Warning("None filter is not implemented")
+      elif newFilter == b'\x01':
+        newRow = reconstructSub(newRow, lastRow, width)
+      elif newFilter == b'\x02':
+        raise Warning("Up filter is not implemented")
+      elif newFilter == b'\x03':
+        newRow = reconstructAvg(newRow, lastRow, width)
+      elif newFilter == b'\x04':
+        newRow = reconstructPaeth(newRow, lastRow, width)
       else:
-        newChunk += colorVal
-      i += 1
-    """
+        raise Warning("Bad filter code")
+      newRowLen = len(newRow)
+      if newRowLen != oldRowLen:
+        print "Ending length of row:", newRowLen
+        print "Debug: Starting length of row:", oldRowLen
+        print "Debug: Row %d Filter: %d" % (i, struct.unpack("B", newFilter[0])[0])
+        raise Warning("Reconstruct didn't provide correct amount of bytes")
+      # Set filter of each line to 0
+      newChunk += struct.pack("B", 0)
+      newChunk += newRow
+      lastRow = newRow
     newLen = len(newChunk)
-    print "New data length:", newLen
+    if newRowLen != oldRowLen:
+      print "Debug: Old data length:", oldLen
+      print "Debug: New data length:", newLen
+      raise Warning("Reconstructed data doesn't contain correct amount of bytes")
     readyData = zlib.compress(newChunk)
     return readyData
 
@@ -139,7 +255,7 @@ def editImageFile(filename, newFilename):
     if chunkType == "IHDR":
       dim = printIHDR(chunkData)
     elif chunkType == "IDAT":
-      chunkData = editColorBytes(chunkData, dim)      
+      chunkData = reconstData(chunkData, dim)      
     print "Writing chunk to file", newFilename
     print ""
     write_chunk(new_fp, chunkType, chunkData)
