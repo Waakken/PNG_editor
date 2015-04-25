35836ed10ab5f080a25fd291b1fc109f626d3c28
Jani Kuutvuori <janikuutvuori@hotmail.com>
2015-04-25 12:04:08 +0300

    Added bytes options. Avg recon is now working

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def setupArgParser():
   parser.add_option("-f", "--filter", action = "store_true", dest="printFilter", help="Print filter information")
   parser.add_option("-d", "--debug", action = "store_true", dest="debug", help="Print debug information")
   parser.add_option("-c", "--chunks", action = "store_true", dest="readChunks", help="Only read chunks from image")
-  parser.add_option("-H", "--hdr", action = "store_true", dest="readHdr", help="Print image data")
+  parser.add_option("-b", "--bytes", dest="readBytes", help="Print first line of color bytes, then exit")
+  parser.add_option("-H", "--hdr", action = "store_true", dest="readHdr", help="Print image data then exit")
   parser.add_option("-r", "--recon", dest="reconFile", \
                     help="Save reconstructed data to this file", metavar="FILE")
   parser.add_option("-e", "--edit", dest="editFile", help="Save edited color data to this file", metavar="FILE")
@@ -38,16 +39,18 @@ def main():
   #pdb.set_trace()
 
   if options.readHdr:
-    p.readImageChunks(args[0], opMode = p.HDR_MODE)
+    p.readChunks(args[0], opMode = p.HDR_MODE)
   if options.printFilter:
-    p.readImageChunks(args[0], opMode = p.FILTER_MODE)
+    p.readChunks(args[0], opMode = p.FILTER_MODE)
   if options.readChunks:
-    p.readImageChunks(args[0], opMode = p.CHUNK_MODE)
+    p.readChunks(args[0], opMode = p.CHUNK_MODE)
+  if options.readBytes:
+    p.readChunks(args[0], bytesLine = int(options.readBytes), opMode = p.BYTES_MODE)
 
   if options.simpleFile:
-    p.readImageChunks(args[0], writeToFile = options.simpleFile, opMode = p.SIMPLE_MODE)
+    p.readChunks(args[0], writeToFile = options.simpleFile, opMode = p.SIMPLE_MODE)
   if options.reconFile:
-    p.readImageChunks(args[0], writeToFile = options.reconFile, opMode = p.RECON_MODE)
+    p.readChunks(args[0], writeToFile = options.reconFile, opMode = p.RECON_MODE)
 
 if __name__ == "__main__":
   main()
